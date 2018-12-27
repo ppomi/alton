16ac1d73f346e7c008e9c9e026cafc3dbd8581f8
권유정 <yoojk150827@gmail.com>
2018-12-27 15:34:13 +0900

    스크롤 위치에 따른 비디오 재생 및 슬라이드 동작 추가

--- js/common.js
+++ js/common.js
@@ -51,23 +51,35 @@ $(function(){
             i=1;
         }
      });
- });
-
- $(function() {
-	var $window = $(window);
-	function doAnimations(){
-		var offset=$window.scrollTop()+$window.height();
-		var $animatables=$('.animatable');
-		if ($animatables.length<0){
-			$window.off('scroll',doAnimations);
-		}
-		$animatables.each(function(i) {
-			var $animatable=$(this);
-			if ($animatable.offset().top+($animatable.height()/2)<offset) {
-				$animatable.removeClass('animatable').addClass('animated');
-			}
-		});
-	}
-	$window.on("scroll", doAnimations);
-	$window.trigger("scroll");
- });
+});
+// 메인비주얼, 스타일 슬라이드 동작
+$(function(){
+    $('.slide_dot').click(function(){
+        var index=$('.slide_dot').index(this);
+        $('.slide_dot').removeClass('active_slide');
+        $('.slide_dot').eq(index).addClass('active_slide');
+        $('.slide_img').removeClass('active_slide');
+        $('.slide_img').eq(index).addClass('active_slide');
+        $('.slide_con').removeClass('active_slide');
+        $('.slide_con').eq(index).addClass('active_slide');
+    });
+    $('.style_list_title').mouseover(function(){
+        var index=$('.style_list_title').index(this);
+        $('.style_img').removeClass('active_slide');
+        $('.style_img').eq(index).addClass('active_slide');       
+    });
+});
+// 비디오 offset값에 따라 재생
+$(function(){
+    var winTop=0;
+    var vidTop=0;
+    $(window).scroll(function() {
+        winTop=$(window).scrollTop();
+        vidTop=$('.alton_video').offset().top-400;
+        if(winTop>vidTop && winTop<vidTop+800){
+            $('#alton_video').get(0).play();
+        }else{
+            $('#alton_video').get(0).pause();
+        }
+    });
+});
