589489b26b70df961ddb796440c5b8b08262127f
권유정 <yoojk150827@gmail.com>
2018-12-27 15:33:32 +0900

    fade-in, pop-out 애니메이션 적용

--- js/common.js
+++ js/common.js
@@ -2,7 +2,33 @@
  * common.js
  */
 
- $(function(){
+// 스크롤 시 div부드럽게 올라오기
+$(function(){
+	var $window = $(window);
+	function anime(){
+		var offset=$window.scrollTop()+$window.height();
+		if ($('.animatable').length<0){
+			$window.off('scroll',anime);
+		}
+		$('.animatable').each(function(i) {
+			var $animatable=$(this);
+			if ($animatable.offset().top+($animatable.height()/2)<offset) {
+				$animatable.removeClass('animatable').addClass('animated');
+			}
+		});
+	}
+	$window.on('scroll', anime);
+	$window.trigger('scroll');
+});
+// top_btn 클릭시 상단 이동
+$(function(){
+    $('#top_btn').click(function() {
+        $("html, body").animate({scrollTop:0}, "slow");
+        return false;
+    });
+});
+// gnb와 util 메뉴 동작
+$(function(){
     var i=0;
      $('.lang_wrap').hide();
      $('.sub_menu').hide();
