05f91cf9dc12ffeea48a667810547f6bad4e162a
imppomi <yoojk150827@gmail.com>
2019-01-14 20:05:19 +0900

    스크립트 수정

--- js/common.js
+++ js/common.js
@@ -14,7 +14,7 @@ $(function(){
 		}
 		$('.animatable').each(function(i) {
 			var $animatable = $(this);
-			if ($animatable.offset().top+($animatable.height()/2)<offset) {
+			if ($animatable.offset().top + ($animatable.height()/2) < offset) {
 				$animatable.removeClass('animatable').addClass('animated');
 			}
 		});
@@ -24,9 +24,8 @@ $(function(){
 });
 // top_btn 클릭시 상단 이동
 $(function(){
-    $('#top_btn').click(function() {
-        $("html, body").animate({scrollTop:0}, "slow");
-        return false;
+    $('.top_btn a').click(function() {
+        $('html, body').animate({'scrollTop' : '0'}, 1000);
     });
 });
 // gnb와 util 메뉴 동작
@@ -35,21 +34,21 @@ $(function(){
      $('.lang_wrap').hide();
      $('.sub_menu').hide();
      $('#search').hide();
-     $('#lang_kor').click(function(){
+     $('a#lang_kor').click(function(){
          $('.lang_wrap').stop().slideToggle();
      });
-     $('.search').click(function(){
+     $('a.search').click(function(){
         if(i == 1){
             $('#search').stop().slideUp();
-            $('.search').css('top','0px');
+            $('.search').css({'top' : '0px'});
             i = 0;
         }else{
             $('#search').stop().slideDown();
-            $('.search').css('top','30px');
+            $('.search').css({'top' : '30px'});
             i = 1;
         }
      });
-     $('#menu_bar').click(function(){
+     $('a#menu_bar').click(function(){
          $('.gnb>ul').toggleClass('on');
          $('.gnb>ul>li').toggleClass('on');
          $('.main_visual').toggleClass('on');
@@ -57,19 +56,15 @@ $(function(){
 });
 // 메인비주얼, 스타일 슬라이드 동작
 $(function(){
-    $('.slide_dot').click(function(){
-        var index = $('.slide_dot').index(this);
-        $('.slide_dot').removeClass('active_slide');
-        $('.slide_dot').eq(index).addClass('active_slide');
-        $('.slide_img').removeClass('active_slide');
-        $('.slide_img').eq(index).addClass('active_slide');
-        $('.slide_con').removeClass('active_slide');
-        $('.slide_con').eq(index).addClass('active_slide');
+    $('.slide_dots a').click(function(){
+        var index = $('.slide_dots a').index(this);
+        $('.slide_dots a').removeClass('active_slide').eq(index).addClass('active_slide');
+        $('.slide_img').removeClass('active_slide').eq(index).addClass('active_slide');
+        $('.slide_con').removeClass('active_slide').eq(index).addClass('active_slide');
     });
-    $('.style_list_title').mouseover(function(){
-        var index = $('.style_list_title').index(this);
-        $('.style_img').removeClass('active_slide');
-        $('.style_img').eq(index).addClass('active_slide');       
+    $('.style_list_title a').mouseover(function(){
+        var index = $('.style_list_title a').index(this);
+        $('.style_img').removeClass('active_slide').eq(index).addClass('active_slide');       
     });
 });
 // 비디오 offset값에 따라 재생
@@ -78,7 +73,7 @@ $(function(){
     var vidTop = 0;
     $(window).scroll(function() {
         winTop = $(window).scrollTop();
-        vidTop = $('.alton_video').offset().top-400;
+        vidTop = $('.alton_video').offset().top - 400;
         if(winTop>vidTop && winTop<vidTop+800){
             $('#alton_video').get(0).play();
         }else{
@@ -91,58 +86,57 @@ $(function(){
 $(function(){
     $('.tab').hide();
     //lineup_dot 클릭시 text복제
-    $('.lineup_dot').click(function(e) {
-        e.preventDefault();
+    $('.lineup_dots a').click(function() {
         $('.tab').hide();
         $('.tab_default').hide();
         var copiedText = $(this).text();
         var copiedIndex = $(this).index();
-        var imgIndex = copiedIndex+1;
-        var imgHtmlContent = '<img src="../img/html/lineup_subvisual_'+imgIndex+'.jpg" alt="알톤자전거 라인업">';
+        var imgIndex = copiedIndex + 1;
+        var imgHtmlContent = '<img src="../img/html/lineup_subvisual_' + imgIndex + '.jpg" alt="알톤자전거 라인업">';
         $('.lineup_img_box').html(imgHtmlContent);        
         $('.lineup_title').html(copiedText);
         $('.tab').eq(copiedIndex).show();
-        if(copiedIndex == 0){
+        if(copiedIndex === 0){
             $('.lineup_des').html('친환경 전기모터의 도움으로 보다 쉬운 주행이 가능한 자전거');
-        }else if(copiedIndex == 1){
+        }else if(copiedIndex === 1){
             $('.lineup_des').html('산과 험로에서 안정적인 주행이 가능한 오프로드');
-        }else if(copiedIndex == 2){
+        }else if(copiedIndex === 2){
             $('.lineup_des').html('일상 속 편안한 주행이 가능한 유사산악자전거형 자전거');
-        }else if(copiedIndex == 3){
+        }else if(copiedIndex === 3){
             $('.lineup_des').html('포장 도로에서 빠른 속도로 주행토록 설계된 자전거');
-        }else if(copiedIndex == 4){
+        }else if(copiedIndex === 4){
             $('.lineup_des').html('산악자전거의 안정감과 로드자전거의 속도감을 더한 일상용');
-        }else if(copiedIndex == 5){
+        }else if(copiedIndex === 5){
             $('.lineup_des').html('보관과 휴대가 편리한 작은 크기의 자전거');
-        }else if(copiedIndex == 6){
+        }else if(copiedIndex === 6){
             $('.lineup_des').html('도시인의 개성있는 라이프 스타일');
-        }else if(copiedIndex == 7){
+        }else if(copiedIndex === 7){
             $('.lineup_des').html('싱글 기어와 심플한 디자인의 스타일리쉬 자전거');
-        }else if(copiedIndex == 8){
+        }else if(copiedIndex === 8){
             $('.lineup_des').html('출퇴근과 주말 레저활동까지 가능한 생활용 자전거');
-        }else if(copiedIndex == 9){
+        }else if(copiedIndex === 9){
             $('.lineup_des').html('미취학 아이들을 위한 보조바퀴가 있는 자전거');
-        }else if(copiedIndex == 10){
+        }else if(copiedIndex === 10){
             $('.lineup_des').html('초등학생들이 타기 적합한 두 발 자전거');
         }else{
             $('.lineup_des').html('기타 자전거');
         }
     });
-    $('.list_view_btn').click(function(){
+    $('.tab_view a.list_view_btn').click(function(){
         $('.grid_view_btn').removeClass('on');
         $(this).addClass('on');
         $('.tab_con').addClass('list_view');
     });
-    $('.grid_view_btn').click(function(){
+    $('.tab_view a.grid_view_btn').click(function(){
         $('.list_view_btn').removeClass('on');
         $(this).addClass('on');
         $('.tab_con').removeClass('list_view');
     });
-    $('.all_sort_btn').click(function(){
+    $('.tab_sort a.all_sort_btn').click(function(){
         $('.price_sort_btn').removeClass('on');
         $(this).addClass('on');
     });
-    $('.price_sort_btn').click(function(){
+    $('.tab_sort a.price_sort_btn').click(function(){
         $('.all_sort_btn').removeClass('on');
         $(this).addClass('on');
     });
@@ -157,26 +151,11 @@ $(function(){
         $('.detail_img .img').removeClass('on');
         $('.detail_img .img').eq(index).addClass('on');
     });
-    $('.detail_part_view_wrapper').each(function(){
-        var pic = $(this).find('img');
-        var count = pic.length;
-        var currentIndex = 0;
-        var sp = 1000;
-        var indicator = $('.detail_part_view_dots a');
-        pic.eq(currentIndex).fadeIn(sp);
-        setInterval(slideShow, 2000);
-        function slideShow(){
-            var nextIndex = (currentIndex+1) % count;
-            pic.eq(currentIndex).fadeOut(sp);
-            pic.eq(nextIndex).fadeIn(sp);
-            currentIndex = nextIndex;
-            indicator.removeClass('on');
-            indicator.eq(currentIndex).addClass('on');
-        }
-        indicator.on('click', function(){
-            if(!$(this).hasClass('on')){
-                slideShow($(this).index());
-            }
-        });
+    $('.detail_part_view_dots a').on('click', function(){
+        var index = $(this).index();
+        $('.detail_part_view_dots a').removeClass('on');
+        $(this).addClass('on');
+        $('.detail_part_view_wrapper img').css({'display' : 'none'});
+        $('.detail_part_view_wrapper img').eq(index).css({'display' : 'block'});
     });
 });
